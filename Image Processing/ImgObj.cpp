477d27018ce7e6332ae728c47571a20fc3a87458
Ali Emre Keskin <aliemrekeskin@outlook.com>
2019-03-19 19:02:52 +0300

    first commit

---
+++ Image Processing/ImgObj.cpp
@@ -0,0 +1,10 @@
+#include "ImgObj.h"
+
+
+
+ImgObj::ImgObj()
+{
+	area = 0;
+	perimeter = 0;
+	thinness = 0;
+}
---
+++ Image Processing/ImgObj.h
@@ -0,0 +1,10 @@
+#pragma once
+ref class ImgObj
+{
+	int area;
+	int perimeter;
+	int thinness;
+public:
+	ImgObj();
+};
+
